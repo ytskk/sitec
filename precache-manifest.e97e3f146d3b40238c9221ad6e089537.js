cd1b72686449a9c6417e3999e59a9a328cf16b3d
ytskk <57366771+ytskk@users.noreply.github.com>
2020-09-07 19:39:07 +0300

    Add files via upload

---
+++ precache-manifest.e97e3f146d3b40238c9221ad6e089537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782f4c253b5f99bdf670233f8816cbbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a10116bb74b5ecd738a",
+    "url": "/static/css/main.4d3ddd2f.chunk.css"
+  },
+  {
+    "revision": "0341c906807f86d123c0",
+    "url": "/static/js/2.db084925.chunk.js"
+  },
+  {
+    "revision": "2364e507b571f95c4bd8e70983dc8a6c",
+    "url": "/static/js/2.db084925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a10116bb74b5ecd738a",
+    "url": "/static/js/main.092beb5a.chunk.js"
+  },
+  {
+    "revision": "0fe4203d9f250db1c2a8",
+    "url": "/static/js/runtime-main.1194f25c.js"
+  },
+  {
+    "revision": "fe6ab62d6aebabfe4f7838bcffa06349",
+    "url": "/static/media/ny.fe6ab62d.jpg"
+  }
+]);
